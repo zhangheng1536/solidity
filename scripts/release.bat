b996944041ca724fbf6fc31f7043dca57f81e2b8
Erik Kundt <bitshift@posteo.org>
2018-06-11 20:44:31 +0200

    Tests if wildcard for VS 2017 version works.

--- scripts/release.bat
+++ scripts/release.bat
@@ -33,10 +33,10 @@ IF "%VERSION%"=="2015" (
     set "DLLS=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x86\Microsoft.VC140.CRT\msvc*.dll"
 ) ELSE (
 
-    IF EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Redist\MSVC\14.14.26405\x86\Microsoft.VC141.CRT\" (
-        set "DLLS=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Redist\MSVC\14.14.26405\x86\Microsoft.VC141.CRT\msvc*.dll"
+    IF EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Redist\MSVC\*\x86\Microsoft.VC141.CRT\" (
+        set "DLLS=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Redist\MSVC\*\x86\Microsoft.VC141.CRT\msvc*.dll"
 		) ELSE (
-        set "DLLS=C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Redist\MSVC\14.14.26405\x86\Microsoft.VC141.CRT\msvc*.dll"
+        set "DLLS=C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Redist\MSVC\*\x86\Microsoft.VC141.CRT\msvc*.dll"
     )
 )
 
